692a5118cbf1bfe6436f178d958a44e6816d03dc
ChinaQY <743282937@qq.com>
2024-06-29 22:24:32 +0800

    Update RDWhitelist.lua

--- RDWhitelist.lua
+++ RDWhitelist.lua
@@ -1,6 +1,7 @@
 return {
     {"CNguxiaowu",false,nil},
     {"DXkuaishou",false,nil},
+    {"yywlike",false,nil},
     {"onlooker01180",false,nil},
     {"poxiaohan520",false,nil},
     {"buhuishuohuadebf",false,nil},
