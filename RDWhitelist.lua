2059584604e112366081a91d5f206150d6ab3017
ChinaQY <743282937@qq.com>
2024-06-29 21:59:05 +0800

    更新 RDWhitelist.lua

--- RDWhitelist.lua
+++ RDWhitelist.lua
@@ -1,6 +1,6 @@
 return {
-    {"CNguxiaowu",false,QY},
-    {"DXkuaishou",false,DX},
+    {"CNguxiaowu",false,"情云"},
+    {"DXkuaishou",false,"DX"},
     {"onlooker01180",false,RJ},
     {"poxiaohan520",false,RJ},
     {"ksqycnm",true,"ban"}
