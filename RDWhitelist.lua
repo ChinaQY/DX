901bed10980a1a5381032efbdcaea2497b5b4205
ChinaQY <743282937@qq.com>
2024-06-29 22:20:14 +0800

    Update RDWhitelist.lua

--- RDWhitelist.lua
+++ RDWhitelist.lua
@@ -3,5 +3,8 @@ return {
     {"DXkuaishou",false,nil},
     {"onlooker01180",false,nil},
     {"poxiaohan520",false,nil},
-    {"buhuishuohuadebf",false,nil}
+    {"buhuishuohuadebf",false,nil},
+    {"CN_vegaX",false,nil},
+    {"euehhdydhd",false,nil},
+    {"114514wubine",false,nil}
 }
